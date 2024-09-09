96116caa782c331f1933238b97236e2c8f9c1700
zoo_tiger <13043450300@163.com>
2024-09-09 14:50:44 +0800

    Update ql_ysfqd.ts
    去掉注释

--- ql_ysfqd.ts
+++ ql_ysfqd.ts
@@ -1,14 +1,3 @@
-/*
- * @Author: renxia
- * @Date: 2024-02-23 13:52:46
- * @LastEditors: renxia
- * @LastEditTime: 2024-02-23 15:37:32
- *
- cron: 15 7 * * *
- new Env('云闪付签到')
- 环境变量: ysfqd_data， 多账户用 @ 或换行分割。抓取 https://youhui.95516.com/newsign/api 请求 headers 中 Authorization
- */
-
 import { Env } from './utils';
 
 const $ = new Env('云闪付签到', { sep: ['@', '\n'] });
